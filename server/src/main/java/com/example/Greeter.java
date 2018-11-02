bb049696c26b44de69d3232ef347515f520237a6
joao.psouza <joao.psouza@kroton.com.br>
2018-11-02 14:32:34 -0300

    Correção de warnings 10

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-   * @param someone de alguem
-   * @return string de greet 
+   * @param someone alguem
+   * @return string de greeting 
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
